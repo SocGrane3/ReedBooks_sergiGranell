625f6815b7e5cbc9bf9f505655954758137aa2bb
Sergi <sergi.granell.7e3@itb.cat>
2021-01-12 22:02:55 +0100

    la llista nomes afeieix en el primer cas

--- ReedBooks/app/src/main/java/cat/itb/reedbooks/ReedbooksListFragment.java
+++ ReedBooks/app/src/main/java/cat/itb/reedbooks/ReedbooksListFragment.java
@@ -26,12 +26,25 @@ public class ReedbooksListFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(booksViewModel == null)booksViewModel = new ViewModelProvider(this).get(BooksViewModel.class);
+        booksViewModel = new ViewModelProvider(this).get(BooksViewModel.class);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if(BooksViewModel.booksregister.size() == 0){
+            BooksViewModel.booksregister.add(new BooksModel("Joc de trons", "George R.R. Martin", "reading", 1, 6));
+            BooksViewModel.booksregister.add(new BooksModel("El archivo de las tormentas", "Brandon Sanderson", "want to read", 0, 6));
+            BooksViewModel.booksregister.add(new BooksModel("Escape Book", "Ivan Tapia", "read", 2, 4));
+            BooksViewModel.booksregister.add(new BooksModel("Endgame", "James Frey", "read", 2, 3));
+            BooksViewModel.booksregister.add(new BooksModel("El nombre del viento", "Patrick Rothfuss", "want to read", 0, 6));
+            BooksViewModel.booksregister.add(new BooksModel("Fi de curs a Bucarest", "Juan Pinyol", "read", 2, 4));
+            BooksViewModel.booksregister.add(new BooksModel("La ma negre", "Jaume Copons", "read", 2, 3));
+            BooksViewModel.booksregister.add(new BooksModel("Escape Book 2", "Ivan Tapia", "want to read", 0, 6));
+            BooksViewModel.booksregister.add(new BooksModel("Tirant Lo Blanc", "Joanot Martorell", "read", 2, 1));
+            BooksViewModel.booksregister.add(new BooksModel("Overlord",  "\tKugane Maruyama", "want to read", 0, 6));
+        }
+
         View v = inflater.inflate(R.layout.reedbooks_list_fragment, container, false);
 
         addButton = v.findViewById(R.id.button_add_list);
